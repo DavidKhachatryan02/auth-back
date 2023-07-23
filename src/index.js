460149e439bfb822413fd1b9468055035aeb552e
David Khachatryan <davo-davo-dav@bk.ru>
2023-07-23 22:30:45 +0400

    hot fix

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ const { errorHandler } = require("./errors");
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use("/auth", authRouter);
 app.use(errorHandler);
 
